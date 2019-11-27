4ce029937c896d7f8b3031eb5169d533cc61ff1b
tattaka <taka_aki@me.com>
2019-11-27 16:08:36 +0900

    Add 1d function

--- filter_response_normalization/filter_response_normalize.py
+++ filter_response_normalization/filter_response_normalize.py
@@ -14,8 +14,43 @@ from torch import nn
 from .replicate import DataParallelWithCallback
 
 __all__ = [
-    'FilterResponseNorm2d', 'convert_model'
+    'FilterResponseNorm1d', 'FilterResponseNorm2d', 'convert_model'
 ]
+
+class FilterResponseNorm1d(nn.Module):
+    _version = 2
+    __constants__ = ['tau', 'beta', 'gamma', 'eps']
+
+    def __init__(self, num_features, eps=1e-5, eps_learnable=True):
+        super( FilterResponseNorm1d, self).__init__()
+        self.num_features = num_features
+        self.tau = nn.parameter.Parameter(torch.Tensor(1, num_features, 1))
+        self.beta = nn.parameter.Parameter(torch.Tensor(1, num_features, 1))
+        self.gamma = nn.parameter.Parameter(torch.Tensor(1, num_features, 1))
+        if eps_learnable: # if eps is learnable parameter
+            self.eps = nn.parameter.Parameter(torch.Tensor(1))
+            nn.init.constant_(self.eps, eps)
+        else:
+            self.eps = eps
+        self.reset_parameters()
+
+
+    def reset_parameters(self):
+        nn.init.zeros_(self.tau)
+        nn.init.zeros_(self.beta)
+        nn.init.ones_(self.gamma)
+        
+    def _check_input_dim(self, input):
+        if input.dim() != 4:
+            raise ValueError('expected 4D input (got {}D input)'
+                             .format(input.dim()))
+#         super(FilterResponseNorm2d, self)._check_input_dim(input)
+
+    def forward(self, input):
+        self._check_input_dim(input)
+        nu2 = torch.mean(input**2, axis=[2], keepdims=True)
+        input = input * torch.rsqrt(nu2 + torch.abs(self.eps))
+        return self.gamma * input + self.beta
 
 class FilterResponseNorm2d(nn.Module):
     _version = 2
@@ -97,12 +132,13 @@ def convert_model(module):
         return mod
 
     mod = module
+    if isinstance(module, torch.nn.modules.batchnorm.BatchNorm1d):
+        mod = FilterResponseNorm1d(module.num_features, module.eps)
     if isinstance(module, torch.nn.modules.batchnorm.BatchNorm2d):
         mod = FilterResponseNorm2d(module.num_features, module.eps)
     elif isinstance(module, torch.nn.ReLU):
-        mod = ThresholdedLinearUnit(inplace = module.inplace)
+        mod = ThresholdedLinearUnit(module.inplace)
     for name, child in module.named_children():
         mod.add_module(name, convert_model(child))
-
     return mod
 
